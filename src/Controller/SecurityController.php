d9ba0b3da29a129b299352a04699b6fd72ae8be0
ferielabd <feriel.abdellatif@esprit.tn>
2022-04-24 05:40:42 +0100

    Gérer_user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,47 +1,50 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\User;
-use App\Form\LoginType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Config\Definition\Exception\Exception;
-/**
- * @Route("/user")
- */
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 class SecurityController extends AbstractController
 {
-
     /**
-     * @Route("/login", name="security_login")
-     * @param AuthenticationUtils $utils
-     * @return Response
+     * @Route("/security", name="app_security")
      */
-    public function login(Request $request,AuthenticationUtils $utils): Response
+    public function index(): Response
     {
-
-
-
-
-        $user = new User();
-        $formL = $this->createForm(LoginType::class, $user);
-        $formL->handleRequest($request);
-        return $this->render('Security/login.html.twig', [
-            'last_username' => $utils->getLastUsername(),
-            'error' => $utils->getLastAuthenticationError(),
-            'formL' => $formL->createView(),
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
         ]);
     }
 
     /**
-     * @Route("/logout", name="security_logout")
-     * @throws Exception
+     * @Route("/login", name="app_login")
+     */
+    public function login(Request $request,AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $user = new User();
+        $formL = $this->createForm(LoginType::class, $user);
+        $formL->handleRequest($request);
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error, 'formL' => $formL->createView()]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
      */
     public function logout(): void
     {
-        throw new Exception('This should never be reached!');
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
